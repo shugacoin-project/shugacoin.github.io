7265f301913ecb238be979e013cc0f3a73470790
Shugacoin <161726771+shugacoin-project@users.noreply.github.com>
2024-03-11 00:39:20 +0700

    update

---
+++ index.php
@@ -0,0 +1,384 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Shugacoin - Open source PoW CPU Blockchain</title>
+	<meta name="description" content="Shugacoin is a cryptocurrency that enables instant payments to anyone in the world and that can be efficiently mined with consumer-grade hardware." />
+	<meta name="keywords" content="shugacoin, shuga, crypto, cryptocurrency, bitcoin, ethereum, monero, btc, eth, miningpoolstats, bitcointalk" />
+	<meta name="author" content="shugacoin"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->
+	<link rel="icon" type="image/png" href="resource/favicon.png" />	
+	<!-- Styles -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css" />
+	<link rel="stylesheet" href="css/icons.css">
+	<link rel="stylesheet" href="css/animate.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="stylesheet" type="text/css" href="css/responsive.css" />
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-SQZ7PDDLLN"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'G-SQZ7PDDLLN');
+	</script>		
+</head>
+<body class="lines" id="scrollup">
+
+<img src="resource/bg/p3.png" class="vx" alt="" data-enllax-ratio="-0.1" data-enllax-type="foreground" />
+
+<div class="theme-layout">
+
+	<header class="sticktop">
+		<div class="menusec">
+			<div class="logo"> 
+				<a href="/" title="shugacoin">
+					<img class="hidec" src="resource/logo/logo-a.png" alt="shugacoin" />
+					<img class="showc" src="resource/logo/logo-b.png" alt="shugacoin" />
+				</a>
+			</div>
+			<div class="apps-btns">
+				<a href="https://wallet.shugacoin.org/#/" class="g1" title="">Web Wallet</a>
+				<a href="https://explorer.shugacoin.org/#/" class="g1" title="">Explorer</a>
+				<a class="smooth" href="#download" title="">Download</a>
+			</div>
+			<div class="open-minimal-menu resopen">
+				<div id="hamburger-two">
+				     <span></span>
+				     <span></span>
+				     <span></span>
+				</div>
+			</div>
+			<nav id="navsc">
+				<div class="apps-btns mobilap">
+					<a href="https://wallet.shugacoin.org/#/" class="g1" title="">Web Wallet</a>
+					<a href="https://explorer.shugacoin.org/#/" class="g1" title="">Explorer</a>
+					<a class="smooth" href="#download" class="g2" title="">Download</a>
+				</div>
+			</nav>
+		</div>
+	</header>
+
+	<section>
+		<div class="block remove-top">
+			<div data-velocity="-.1" style="background: url(resource/bg/bakcground.png) repeat scroll 50% 422.28px transparent;" class="parallax no-parallax scrolly-invisible"></div>
+			<div class="container fluid">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="fsec">
+							<div class="finfos wow fadeIn" data-wow-delay="0s">
+								<h3>SHUGACOIN</h3>
+								<p>one-CPU-one-vote, PoW blockchain</p>
+								<div class="social">
+									<a href="https://twitter.com/shugacoin" title=""><i class="fa fa-twitter"></i></a>
+									<a href="https://t.me/shugacoin" title=""><i class="fa fa-telegram"></i></a>
+									<a href="https://bitcointalk.org/index.php?topic=5487986.0" title=""><i class="fa fa-bitcoin"></i></a>
+									<a href="https://github.com/shugacoin-project" title=""><i class="fa fa-github"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section>
+		<div class="block remove-top">
+			<div class="container fluid expadding">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="countsec">
+							<h3 class=" wow fadeIn" data-wow-delay="0s">Information Blockchain</h3>
+						</div>					
+						<ul class="someblock">
+							<li class="wow fadeIn" data-wow-delay="0s">
+								<span>Current Height</span>
+								<b><p id="listblocks"><a href="#" class="current">loading...</a></p></b>
+							</li>
+							<li class="wow fadeIn" data-wow-delay="0.2s">
+								<span>Reward</span>
+								<b><p id="reward">loading...</p></b>
+							</li>
+							<li class=" wow fadeIn" data-wow-delay="0.4s">
+								<span>Network Hashrate</span>
+								<b><p id="netHashrate">loading...</p></b>
+							</li>
+						</ul>
+						<ul class="someblock">
+							<li class="wow fadeIn" data-wow-delay="0.6s">
+								<span>Remaining Blocks Left</span>
+								<b><p id="blkleft">loading...</p></b>
+							</li>
+							<li class=" wow fadeIn" data-wow-delay="0.8s">
+								<span>Total Shugacoin Left to Mine</span>
+								<b><p id="shugaLeft">loading...</p></b>
+							</li>
+							<li class=" wow fadeIn" data-wow-delay="0.10s">
+								<span>Left until halving</span>
+								<b><p id="hours">loading...</p></b>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="download">
+		<div class="block remove-top less-bottom">
+			<div class="container fluid expadding">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="countsec">
+							<h3 class=" wow fadeIn" data-wow-delay="0s">Download</h3>
+							<p class=" wow fadeIn" data-wow-delay="0.2s">Download Qt Core Wallet Shugacoin.</p>
+							<div class="social wow fadeIn" data-wow-delay="0.8s">
+								<a href="https://github.com/shugacoin-project/shugacoin/releases/latest" title=""><i class="fa fa-windows"></i></a>
+								<a href="https://github.com/shugacoin-project/shugacoin/releases/latest" title=""><i class="fa fa-linux"></i></a>
+								<a href="https://github.com/shugacoin-project/shugacoin/releases/latest" title=""><i class="fa fa-apple"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="three">
+		<div class="block">
+			<div data-velocity="-.1" style="background: url(resource/bg/p3.png) repeat scroll 50% 422.28px transparent;" class="parallax scrolly-invisible"></div><!-- PARALLAX BACKGROUND IMAGE -->	
+			<div class="container fluid">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="roadmapsec">
+							<h3>Road Map</h3>
+							<div class="roadnavsec">
+								<ul class="roadnav">
+							 		<li><span>Q1 2024</span></li>
+							 		<li><span>Q2 2024</span></li>
+									<!--
+							 		<li><span>Q3</span></li>
+							 		<li><span>Q4</span></li>
+							 		<li><span>Q5</span></li>
+							 		<li><span>Q6</span></li>
+							 		<li><span>Q7 </span></li>
+									-->
+							 	</ul>
+							 </div>
+						 	<ul class="roadcontent">
+						 		<li>
+						 			<div class="rmcontent">
+							 			<div class="roadmap wow fadeInUp" data-wow-delay="0s"><i class="cl1"></i><h3>Q1</h3><p>Start Built from source code.</p></div><!-- RoadMap -->
+							 			<div class="roadmap wow fadeInUp" data-wow-delay="0.2s"><i class="cl2"></i><h3>Q1.1</h3> <p>Built social community</p> </div><!-- RoadMap -->
+							 			<div class="roadmap wow fadeInUp" data-wow-delay="0.4s"><i class="cl3"></i><h3>Q1.2</h3> <p>Start Testnet blockchain</p> </div><!-- RoadMap -->
+							 			<div class="roadmap wow fadeInUp" data-wow-delay="0.6s"><i class="cl4"></i><h3>Q1.3</h3> <p>Start Mainnet blockchain</p> </div><!-- RoadMap -->
+							 			<div class="roadmap wow fadeInUp" data-wow-delay="0.8s"><i class="cl5"></i><h3>Q1.4</h3> <p>Launch on github, twitter/x, telegram , bitcointalk</p> </div><!-- RoadMap -->
+						 			</div>
+						 		</li>
+						 		<li>
+						 			<div class="rmcontent">
+							 			<div class="roadmap wow fadeInUp" data-wow-delay="0s"><i class="cl1"></i><h3>Q2</h3><p>launch web wallet</p></div><!-- RoadMap -->
+							 			<div class="roadmap wow fadeInUp" data-wow-delay="0.4s"><i class="c12"></i><h3>Q2.2</h3> <p>First Exchange of shugacoin</p> </div><!-- RoadMap -->
+							 			<div class="roadmap wow fadeInUp" data-wow-delay="0.2s"><i class="c13"></i><h3>Q2.1</h3> <p>Built mobile apps wallet</p> </div><!-- RoadMap -->
+							 			<div class="roadmap wow fadeInUp" data-wow-delay="0.6s"><i class="cl4"></i><h3>Q2.3</h3> <p>TBA</p> </div><!-- RoadMap -->
+							 			<div class="roadmap wow fadeInUp" data-wow-delay="0.8s"><i class="cl5"></i><h3>Q2.4</h3> <p>TBA</p> </div><!-- RoadMap -->
+						 			</div>
+						 		</li>
+						 	</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<section id="six">
+		<div class="block">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-lg-7">
+						<div class="faqsec">
+							<h3>Features</h3>
+							<div id="toggle-widget" class="experties">
+								<h2 class="wow fadeInUp" data-wow-delay="0s">Open Source Software</h2>
+								<div class="content">
+									<p>Shugacoin is an open source software project released under the MIT/X11 license which gives you the power to run, modify, and copy the software and to distribute, at your option, modified copies of the software. The software is released in a transparent process that allows for independent verification of binaries and their corresponding source code.</p>
+								</div>
+								<h2 class="wow fadeInUp" data-wow-delay="0.4s">Wallet Encryption</h2>
+								<div class="content">
+									<p>Wallet encryption allows you to secure your wallet, so that you can view transactions and your account balance, but are required to enter your password before spending SHUGA. This provides protection from wallet-stealing viruses and trojans as well as a sanity check before sending payments.</p>
+								</div>
+								<h2 class="wow fadeInUp" data-wow-delay="0.6s">Mining Reward</h2>
+								<div class="content">
+									<p>Miners are currently awarded with 42.94967296 SHUGA per block, an amount which gets halved roughly every 2 years (every 12,500,000 blocks).</p>
+								</div>
+								<h2 class="wow fadeInUp" data-wow-delay="0.6s">Halving interval</h2>
+								<div class="content">
+									<p><b>12,500,000</b> Blocks (approx. 2 years)</p>
+								</div>
+								<h2 class="wow fadeInUp" data-wow-delay="0.6s">Block time</h2>
+								<div class="content">
+									<p><b>5</b> Seconds</p>
+								</div>
+								<h2 class="wow fadeInUp" data-wow-delay="0.6s">PoW algorithm</h2>
+								<div class="content">
+									<p>YespowerShuga (based on Yespower 1.0.1)</p>
+								</div>
+								<h2 class="wow fadeInUp" data-wow-delay="0.6s">Difficulty</h2>
+								<div class="content">
+									<p>ShugaShield-N510 (based on Zcash's modification of Digishield)</p>
+								</div>
+								<h2 class="wow fadeInUp" data-wow-delay="0.6s">Use Port</h2>
+								<div class="content">
+									<p>40001 / RPC 40002</p>
+								</div>								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="seven">
+		<div class="block remove-bottom gray">
+			<div class="bottomline">
+				<div class="scrollup"><a href="#scrollup" title="">Scroll Up</a></div>
+				<span>Copyright &copy; 2024 shugacoin.org All rights reserved. Built with <i class="fa fa-heart"></i></span>
+			</div>
+		</div>
+	</section>
+
+</div>
+
+
+<script src="js/jquery.min.js" type="text/javascript"></script>
+<script src="js/modernizr.js" type="text/javascript"></script>
+<script src="js/script.js" type="text/javascript"></script>
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
+<script src="js/wow.min.js" type="text/javascript"></script>
+<script src="js/slick.min.js" type="text/javascript"></script>
+<script src="js/scrolly.js" type="text/javascript"></script>
+<script src="js/countdown.js" type="text/javascript"></script>
+<script src="js/mouse.js" type="text/javascript"></script>
+<script src="js/3d.js" type="text/javascript"></script>
+<script src="js/enllax.js" type="text/javascript"></script>
+<script src="js/poptrox.js" type="text/javascript"></script>
+<script src="js/scrollnav.js" type="text/javascript"></script>
+<script src="js/scrollup.js" type="text/javascript"></script>
+        <script>
+            api = "https://api.shugacoin.org"
+
+            function getPrice() {
+                return Promise.resolve($.ajax({
+                    url: api + "/price",
+                    dataType: 'json',
+                    type: 'GET',
+                }))
+            }
+
+            function getBlockHeight() {
+                return Promise.resolve($.ajax({
+                    url: api + "/info",
+                    dataType: "json",
+                    type: "GET"
+                }))
+            }
+
+            function printData() {
+                getBlockHeight().then(function(data) {
+                    var height = data.result.blocks
+					$("#listblocks .current").attr("href", "https://explorer.shugacoin.org/#/block/" + data.result.bestblockhash)
+                    $("#listblocks .current").text(height)
+                })
+            }
+
+            function getTime() {
+                getBlockHeight().then(function(data) {
+                    var height = data.result.blocks
+                    var time = height * 5
+                })
+            }
+            function getBlockReward() {
+                getBlockHeight().then(function(data) {
+                    var getReward = parseInt(data.result.reward)
+                    var blockreward = getReward / 100000000
+                    $("#reward").text(blockreward)
+                })
+            }
+            function secondsToHms(d) {
+                d = Number(d);
+                
+                var hours = Math.floor(d / 3600);
+                var m = Math.floor(d % 3600 / 60);
+                var s = Math.floor(d % 3600 % 60);
+                var day = Math.floor(hours / 24)
+                var h = hours - (day * 24)
+
+                return (day) + " Days, " + (h) + 'h' +":" + ('0' + m).slice(-2) + 'm' + ":" + ('0' + s).slice(-2) + 's';
+            }
+            n = 1
+            function getRemainingBlocksLeft() {
+                getBlockHeight().then(function(data) {
+                half = 12500000 * n
+                if(document.getElementById("blkleft").innerHTML == "0") {
+                    n++;
+                } else if (document.getElementById("blkleft").innerHTML > "0") {
+                    n;
+                }
+                height = data.result.blocks
+                remainheight = half-height
+                $("#blkleft").text(remainheight)
+                $("#hours").text(secondsToHms(remainheight * 5))
+                })
+                }
+
+            function getNetDifficulty() {
+                getBlockHeight().then(function(data) {
+                    getnetDif = parseFloat(data.result.difficulty)
+                    $("#netDifficulty").text(getnetDif)
+                })
+            }
+            
+            function getNetHash() {
+                getBlockHeight().then(function(data) {
+                    netHash = Math.round(((data.result.nethash) / 1000000) * 100) / 100
+                    $("#netHashrate").text(netHash + " MH/S")
+                })
+            }
+            
+            function getAmountShugaLeft() {
+                amountLeft = Math.round((document.getElementById("blkleft").innerHTML) * (document.getElementById("reward").innerHTML))
+                $("#shugaLeft").text(amountLeft)
+            }
+
+            function getCurrentPrice() {
+                getPrice().then(function(data) {
+                    priceUSD = parseFloat(data.result.usd)
+                    getpriceBTC = data.result.btc
+                    priceBTC = Number(getpriceBTC).toLocaleString(undefined, {minimumFractionDigits: 8, maximumFractionDigits: 8})
+                    $("#priceusd").text("$" + priceUSD)
+                    $("#pricebtc").text(priceBTC + "BTC")
+
+                })
+            }
+
+            setInterval(function() {
+                printData()
+                getTime()
+                getBlockReward()
+                getRemainingBlocksLeft()
+                getNetDifficulty()
+                getNetHash()
+                getAmountShugaLeft()
+                getCurrentPrice()	
+            }, 1000)
+        </script>	
+</body>
+</html>
+
